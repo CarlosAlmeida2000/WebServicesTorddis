cbb26a9af5b8edad85ebcab726d2175f11019042
Carlosalmeida2000 <carlos.almeida2017@uteq.edu.ec>
2023-06-01 21:28:38 -0500

    Habilitanto el uso de la camara del dispositivo

--- Monitoreo/entrenamiento_facial.py
+++ Monitoreo/entrenamiento_facial.py
@@ -32,58 +32,58 @@ class EntrenamiFacial:
             os.makedirs(self.ruta_rostros + '\\' + str(self.supervisado.pk), exist_ok = True)
 
             # ***** Código para usar la CAMARA del dispositivo
-            # camara_ip = Camaras.objects.get(tutor_id = self.tutor_id).direccion_ruta
-            # stream = urlopen('http://'+ camara_ip +':81/stream')
+            camara_ip = Camaras.objects.get(tutor_id = self.tutor_id).direccion_ruta
+            stream = urlopen('http://'+ camara_ip +':81/stream')
             # ***** fin
 
             # ***** Código para usar CAMARA WEB
-            cap = cv2.VideoCapture(0)
+            # cap = cv2.VideoCapture(0)
             # ***** fin
             
             while self.cont_imagenes < self.imagenes_capturar:
 
                 # ***** Código para usar la CAMARA del dispositivo
-                # self.byte += stream.read(4096)
-                # alto_imagen = self.byte.find(b'\xff\xd8')
-                # ancho_imagen = self.byte.find(b'\xff\xd9')
-                # if alto_imagen != -1 and ancho_imagen != -1:
-                #     imagen = self.byte[alto_imagen:ancho_imagen + 2]
-                #     self.byte = self.byte[ancho_imagen + 2:]
-                #     if imagen:
+                self.byte += stream.read(4096)
+                alto_imagen = self.byte.find(b'\xff\xd8')
+                ancho_imagen = self.byte.find(b'\xff\xd9')
+                if alto_imagen != -1 and ancho_imagen != -1:
+                    imagen = self.byte[alto_imagen:ancho_imagen + 2]
+                    self.byte = self.byte[ancho_imagen + 2:]
+                    if imagen:
                 # ***** fin
 
                 # ***** Código para usar CAMARA WEB
-                ret, imagen = cap.read()
-                if not ret:
-                    break
+                # ret, imagen = cap.read()
+                # if not ret:
+                #     break
                 # ***** fin
 
                         # ***** Código para usar la CAMARA del dispositivo
-                        # self.video = cv2.imdecode(np.fromstring(imagen, dtype = np.uint8), cv2.IMREAD_COLOR)
-                        # self.video = cv2.resize(self.video, (1490, 760), interpolation = cv2.INTER_CUBIC)
+                        self.video = cv2.imdecode(np.fromstring(imagen, dtype = np.uint8), cv2.IMREAD_COLOR)
+                        self.video = cv2.resize(self.video, (1490, 760), interpolation = cv2.INTER_CUBIC)
                         # ***** fin
 
                 # ***** Código para usar CAMARA WEB
-                self.video = cv2.resize(imagen, (1490, 760), interpolation = cv2.INTER_CUBIC)
+                # self.video = cv2.resize(imagen, (1490, 760), interpolation = cv2.INTER_CUBIC)
                 # ***** fin
 
 
-                gray = cv2.cvtColor(self.video, cv2.COLOR_BGR2GRAY)
-                auxFrame = self.video.copy()
-                rostros = self.clasificador_haar.detectMultiScale(gray, 1.3, 5)
-                self.estado = '{0} fotos de {1}'.format((self.cont_imagenes + 1), self.imagenes_capturar)
-                cv2.putText(self.video, self.estado, 
-                (20, 28), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
-                for (x, y, w, h) in rostros:
-                    cv2.rectangle(self.video, (x, y),(x + w, y + h),(0, 255, 0), 2)
-                    rostro = cv2.resize(auxFrame[y:y + h, x:x + w],(150, 150),interpolation = cv2.INTER_CUBIC)
-                    cv2.imwrite(self.ruta_rostros + '\\' + str(self.supervisado.pk) + '/rotro_{}.png'
-                    .format(self.cont_imagenes), rostro)
-                    self.cont_imagenes += 1
-                cv2.imshow('Video', self.video)
-                k =  cv2.waitKey(1)
-                if k == 27:
-                    break
+                        gray = cv2.cvtColor(self.video, cv2.COLOR_BGR2GRAY)
+                        auxFrame = self.video.copy()
+                        rostros = self.clasificador_haar.detectMultiScale(gray, 1.3, 5)
+                        self.estado = '{0} fotos de {1}'.format((self.cont_imagenes + 1), self.imagenes_capturar)
+                        cv2.putText(self.video, self.estado, 
+                        (20, 28), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+                        for (x, y, w, h) in rostros:
+                            cv2.rectangle(self.video, (x, y),(x + w, y + h),(0, 255, 0), 2)
+                            rostro = cv2.resize(auxFrame[y:y + h, x:x + w],(150, 150),interpolation = cv2.INTER_CUBIC)
+                            cv2.imwrite(self.ruta_rostros + '\\' + str(self.supervisado.pk) + '/rotro_{}.png'
+                            .format(self.cont_imagenes), rostro)
+                            self.cont_imagenes += 1
+                        cv2.imshow('Video', self.video)
+                        k =  cv2.waitKey(1)
+                        if k == 27:
+                            break
 
             cv2.destroyAllWindows()
             self.fin_entrenamiento = True
